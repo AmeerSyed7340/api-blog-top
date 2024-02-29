4a8a0dfa41af78275a57ca30982424b045dc0fb6
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-02-28 21:42:46 -0500

    Set up app.js as the entry point which runs successfully

--- server/app.js
+++ server/app.js
@@ -1,1 +1,11 @@
-console.log("hello");
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Express server running...')
+})
+
+app.listen(3000, () => {
+    console.log('Server listening on PORT: 3000');
+});
